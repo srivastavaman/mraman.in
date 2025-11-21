a4a7157afb53adf65897c8df73b1f3e1b451f3de
srivastavaman <amansrivastava3516@gmail.com>
2025-11-21 13:37:38 +0530

    remove admin section

--- src/app/api/admin-disable/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const res = NextResponse.redirect(new URL("/admin", process.env.NEXT_PUBLIC_BASE_URL));
-  res.cookies.set("admin-auth", "", { maxAge: 0 }); // clear cookie
-  return res;
-}
